99490eac608b979a4eb523c20d23a8e5e2aa2554
Marcellus Saputra <marcellussaputra@gmail.com>
2023-03-30 17:44:22 +0200

    first commit

---
+++ lib/screens/product_details_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ProductDetailsScreen extends StatelessWidget {
+  static const routeName = '/product-detail';
+  @override
+  Widget build(BuildContext context) {
+    final productId = ModalRoute.of(context).settings.arguments as String;
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
